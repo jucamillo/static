0e21ac72ce285cb4d102fc6bf43487fdd87ca4ab
php 04 <php.04@xuxu.cec.ime.usp.br>
2018-01-24 22:38:33 -0200

    contador

---
+++ conta.php
@@ -0,0 +1,23 @@
+<?php
+
+class conta
+{
+  public $numero;
+  public static $contador = 0; //atributo de classe
+
+  //funcao que é chamada sempre que um aluno é criada (construtor)
+
+  //se coloca um parametro dentro de um construtor esse parametro se torna 'obrigatorio', ele nao deixa de criar, ele apenas da um aviso
+// so da erro se pedir o tipo, p pedir o tipo tem que add o declare la em cima
+  function __construct($numero)
+  {
+    $this->numero = $numero;
+    self::$contador++;
+    echo "Conta $numero / Contador:" . conta::$contador . PHP_EOL;
+  }
+}
+echo "Contador" . conta::$contador . PHP_EOL;
+$conta = new conta(8982392);
+$conta = new conta(4444);
+$conta = new conta(222);
+$conta = new conta(111);
